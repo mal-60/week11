4e8621e250d45e56045fbeec80a5167f1fcde142
Anh <minhanhle@hotmail.com>
2022-11-15 09:48:17 -0800

    function practice

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+
+// function greet (UserName) 
+// {
+// console.log(userName) 
+// // console.log(`Hello ${userName}`);  
+// }
+
+
+let greet = function (userName) {
+    // console.log("Hello" + userName);
+    console.log(`Hello ${userName}`);
+};                                         // This one has a semi colon after the }
+
+greet("Anh");
